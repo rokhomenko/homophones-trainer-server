2604a071118ddfd9eb0282ef65c4bbf08a83b25b
rokhomenko <roksa.khom@gmail.com>
2025-12-16 00:45:55 +0100

    app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { PrismaModule } from './prisma/prisma.module'
+import { WordsModule } from './words/words.module'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule,
+    WordsModule,
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
